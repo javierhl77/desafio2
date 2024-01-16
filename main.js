e0590cf48e21d0ed195bc9af325d423e2ef6fd19
javierhl77 <tupackatari1977@gmail.com>
2024-01-16 10:28:27 -0300

    falta metodo update

--- main.js
+++ main.js
@@ -44,19 +44,57 @@ class ProductManager {
     await fs.writeFile( this.patch, JSON.stringify(this.products))
 
   }
+  readProducts = async () => {
+    let respuesta = await fs.readFile(this.patch, "utf-8")
+    return JSON.parse(respuesta);
+  }
 
   getProducts = async ()  => {
 
-    let respuesta = await fs.readFile(this.patch, "utf-8");
-    console.log(JSON.parse(respuesta))
-  }
+    let respuesta1 = await this.readProducts()
+    return console.log(respuesta1)  }
 
   getProductsById = async (id) => {
-
-     
+     let respuesta2 = await this.readProducts()
+     let filter = respuesta2.find(product => product.id === id)
+     //console.log(filter)
+     if ( !respuesta2.find(product => product.id === id) ) {
+      console.log("no existe el producto")
+     } else {
+      console.log(filter)
+     }
   }
+
+
+  deleteProductById = async (id ) => {
+    let resp = await this.readProducts();
+    let prodFilter = resp.filter(products => products.id != id)
+    await fs.writeFile( this.patch, JSON.stringify(prodFilter))
+
+    console.log("producto eliminado");
+  }
+  updateProducts = async (producto ) =>  {
+   console.log(producto)
+
+  }
+   
+
+
 }
 const products = new ProductManager;
 products.addProduct("doña paula","tinto malbec",3500,"imagen","abc124",24);
+products.addProduct("aminga","tinto malbec",3500,"imagen","abc1245",24);
 
-products.getProducts();
+//products.getProducts();
+//products.getProductsById(2);
+//products.deleteProductById(1);
+
+products.updateProducts( {
+  "title":"aminga",
+"description":"tinto malbec",
+"price":6500,"img":"imagen",
+"code":"abc1245",
+"stock":24,
+"id":2
+
+})
